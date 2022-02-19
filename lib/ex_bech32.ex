43d7438a4a3e90f22c653fafaf42485207f1e4a9
Ayrat Badykov <ayratin555@gmail.com>
2022-02-19 18:03:02 +0200

    initial commit

---
+++ lib/ex_bech32.ex
@@ -0,0 +1,18 @@
+defmodule ExBech32 do
+  @moduledoc """
+  Documentation for `ExBech32`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBech32.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_bech32_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBech32Test do
+  use ExUnit.Case
+  doctest ExBech32
+
+  test "greets the world" do
+    assert ExBech32.hello() == :world
+  end
+end
