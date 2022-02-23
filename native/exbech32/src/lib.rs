783fa68695f88d51d86e8cae91a6f9127bd302fd
Ayrat Badykov <ayratin555@gmail.com>
2022-02-23 19:54:01 +0200

    add function to decode/encode with version
    version is required for bech32 addresses

--- native/exbech32/src/lib.rs
+++ native/exbech32/src/lib.rs
@@ -19,24 +19,52 @@ mod atoms {
 }
 
 #[rustler::nif]
-fn encode<'a>(env: Env<'a>, hrp: String, data: Binary, string_variant: String) -> Term<'a> {
-    let variant = match string_to_variant(env, string_variant) {
-        Ok(var) => var,
-        Err(err) => return err,
-    };
-
+fn encode_with_version<'a>(
+    env: Env<'a>,
+    hrp: String,
+    version: u8,
+    data: Binary,
+    string_variant: String,
+) -> Term<'a> {
     let mut u5_vec = data.as_slice().to_base32();
-    u5_vec.insert(0, u5::try_from_u8(1).unwrap());
+    u5_vec.insert(0, u5::try_from_u8(version).unwrap());
 
-    match bech32::encode(&hrp, u5_vec, variant) {
-        Err(_) => (atoms::error(), atoms::encode_error()).encode(env),
-        Ok(encoded) => (atoms::ok(), encoded.to_string()).encode(env),
+    do_encode(env, &hrp, u5_vec, string_variant)
+}
+
+#[rustler::nif]
+fn encode<'a>(env: Env<'a>, hrp: String, data: Binary, string_variant: String) -> Term<'a> {
+    let u5_vec = data.as_slice().to_base32();
+
+    do_encode(env, &hrp, u5_vec, string_variant)
+}
+
+#[rustler::nif]
+fn decode_with_version<'a>(env: Env<'a>, encoded: String) -> Term<'a> {
+    match do_decode(env, encoded) {
+        Ok((hrp, base32_data, variant)) => {
+            let mut actual_data = Vec::<u8>::from_base32(&base32_data).unwrap();
+            let version = actual_data.split_off(1);
+
+            let mut erl_bin: OwnedBinary = OwnedBinary::new(actual_data.len()).unwrap();
+            erl_bin.as_mut_slice().copy_from_slice(&actual_data);
+
+            let string_variant = variant_to_string(variant);
+
+            (
+                atoms::ok(),
+                (hrp, version[0], erl_bin.release(env), string_variant),
+            )
+                .encode(env)
+        }
+
+        Err(error) => return error,
     }
 }
 
 #[rustler::nif]
 fn decode<'a>(env: Env<'a>, encoded: String) -> Term<'a> {
-    match bech32::decode(&encoded) {
+    match do_decode(env, encoded) {
         Ok((hrp, base32_data, variant)) => {
             let slice_data = Vec::<u8>::from_base32(&base32_data).unwrap();
 
@@ -48,8 +76,30 @@ fn decode<'a>(env: Env<'a>, encoded: String) -> Term<'a> {
             (atoms::ok(), (hrp, erl_bin.release(env), string_variant)).encode(env)
         }
 
-        Err(_) => (atoms::error(), atoms::decode_error()).encode(env),
+        Err(error) => return error,
     }
+}
+
+fn do_encode<'a>(env: Env<'a>, hrp: &str, data: Vec<u5>, string_variant: String) -> Term<'a> {
+    let variant = match string_to_variant(env, string_variant) {
+        Ok(var) => var,
+        Err(err) => return err,
+    };
+
+    match bech32::encode(hrp, data, variant) {
+        Err(_) => (atoms::error(), atoms::encode_error()).encode(env),
+        Ok(encoded) => (atoms::ok(), encoded.to_string()).encode(env),
+    }
+}
+
+fn do_decode<'a>(env: Env<'a>, encoded: String) -> Result<(String, Vec<u5>, Variant), Term<'a>> {
+    let decoded = bech32::decode(&encoded);
+
+    if let Err(_) = decoded {
+        return Err((atoms::error(), atoms::decode_error()).encode(env));
+    }
+
+    Ok(decoded.unwrap())
 }
 
 fn variant_to_string(variant: Variant) -> String {
@@ -67,4 +117,7 @@ fn string_to_variant<'a>(env: Env<'a>, string_variant: String) -> Result<Variant
     }
 }
 
-rustler::init!("Elixir.ExBech32.Impl", [encode, decode]);
+rustler::init!(
+    "Elixir.ExBech32.Impl",
+    [encode, decode, encode_with_version, decode_with_version]
+);
