904fb539a23a6d01da9f2badf0848eefd5bc4a05
Ayrat Badykov <ayratin555@gmail.com>
2022-02-26 10:23:13 +0200

    update rustler to 0.24

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule ExBech32.MixProject do
 
   defp deps do
     [
-      {:rustler, "~> 0.23"},
+      {:rustler, "~> 0.24"},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
