5fdddf58d0d0dfad6db0cc6175c1041362a9e911
Ayrat Badykov <ayratin555@gmail.com>
2023-03-25 16:52:57 +0200

    precompiled rust

--- lib/ex_bech32/impl.ex
+++ lib/ex_bech32/impl.ex
@@ -1,6 +1,12 @@
 defmodule ExBech32.Impl do
   @moduledoc false
-  use Rustler, otp_app: :ex_bech32, crate: :exbech32
+
+  use RustlerPrecompiled,
+    otp_app: :ex_bech32,
+    crate: :exbech32,
+    base_url: "https://github.com/ayrat555/ex_bech32/releases/download/v#{version}",
+    force_build: System.get_env("EX_BECH32_BUILD") in ["1", "true"],
+    version: version
 
   def encode(_hrp, _data, _variant), do: :erlang.nif_error(:nif_not_loaded)
 
--- mix.exs
+++ mix.exs
@@ -3,11 +3,13 @@ defmodule ExBech32.MixProject do
 
   @source_url "https://github.com/ayrat555/ex_bech32"
 
+  @version "0.5.0"
+
   def project do
     [
       app: :ex_bech32,
       name: "ExBech32",
-      version: "0.4.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -24,10 +26,11 @@ defmodule ExBech32.MixProject do
 
   defp deps do
     [
-      {:rustler, "~> 0.26"},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:rustler, ">= 0.0.0", optional: true},
+      {:rustler_precompiled, "~> 0.6"}
     ]
   end
 
