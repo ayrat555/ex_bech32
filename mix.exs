f040a0f6a2862a53945cf5b8456a79d928b497b6
Ayrat Badykov <ayratin555@gmail.com>
2023-09-06 11:28:44 +0300

    update rustler to 0.29

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExBech32.MixProject do
 
   @source_url "https://github.com/ayrat555/ex_bech32"
 
-  @version "0.5.0"
+  @version "0.5.1"
 
   def project do
     [
@@ -26,8 +26,8 @@ defmodule ExBech32.MixProject do
 
   defp deps do
     [
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:rustler, ">= 0.0.0", optional: true},
       {:rustler_precompiled, "~> 0.6"}
