f1bc7e3847a289516320beda5c5880d29a3d72c9
Ayrat Badykov <ayratin555@gmail.com>
2023-09-06 11:33:24 +0300

    update rustler to 0.29 (#27)
    * update rustler to 0.29
    
    * update mix.lock
    
    * fix ci

--- lib/ex_bech32/impl.ex
+++ lib/ex_bech32/impl.ex
@@ -7,7 +7,8 @@ defmodule ExBech32.Impl do
     otp_app: :ex_bech32,
     crate: :ex_bech32,
     base_url: "https://github.com/ayrat555/ex_bech32/releases/download/v#{version}",
-    force_build: System.get_env("EX_BECH32_BUILD") in ["1", "true"],
+    force_build: System.get_env("RUSTLER_BUILD") in ["1", "true"],
+    targets: Enum.uniq(["x86_64-unknown-freebsd" | RustlerPrecompiled.Config.default_targets()]),
     version: version
 
   def encode(_hrp, _data, _variant), do: :erlang.nif_error(:nif_not_loaded)
