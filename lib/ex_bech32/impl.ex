1b58bffb006ae0e5e248707cc9c412bdc3dfdaaf
Ayrat Badykov <ayratin555@gmail.com>
2024-04-15 11:53:20 +0300

    update ci workflow

--- lib/ex_bech32/impl.ex
+++ lib/ex_bech32/impl.ex
@@ -9,6 +9,7 @@ defmodule ExBech32.Impl do
     base_url: "https://github.com/ayrat555/ex_bech32/releases/download/v#{version}",
     force_build: true,
     targets: Enum.uniq(["x86_64-unknown-freebsd" | RustlerPrecompiled.Config.default_targets()]),
+    nif_versions: ["2.15", "2.16"],
     version: version
 
   def encode(_hrp, _data, _variant), do: :erlang.nif_error(:nif_not_loaded)
